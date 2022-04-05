96591bd2025ee381530a3e013cfe5b9c741af1cd
DAM108 <dam08.2022@gmail.com>
2022-04-05 08:52:03 +0200

    Proyecto documentado

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"examen","c":"Hora","l":"Hora()","u":"%3Cinit%3E()"},{"p":"examen","c":"Hora","l":"horaValida"},{"p":"examen","c":"Hora","l":"validarHora(int, int, int)","u":"validarHora(int,int,int)"}];updateSearchResults();
--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,7 +1,21 @@
 package examen;
+/**
+ * Clase que dice que el formato de hora es valido
+ * 
+ * @author DAM108
+ * @version "1.0"
+ */
 
 public class Hora {
+
 	public static boolean horaValida;
+/**
+ * 
+ * @param segundos variable para los segundos
+ * @param minutos variable para los minutos
+ * @param horas variable para las horas
+ * @return true si el formato es correcto, false si no lo es 
+ */
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		horaValida = true;
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
