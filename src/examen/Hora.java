d1d9c5858e8e853ceaa1af78e5f03f881ead29fa
DAM108 <dam08.2022@gmail.com>
2022-04-05 08:37:30 +0200

    Proyecto sin documentar

---
+++ src/examen/Hora.java
@@ -0,0 +1,21 @@
+package examen;
+
+public class Hora {
+	public static boolean horaValida;
+
+	public static boolean validarHora(int segundos, int minutos, int horas) {
+		horaValida = true;
+		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
+			if (horas > 23) {
+				horaValida = false;
+			} else if (minutos > 59) {
+				horaValida = false;
+			} else if (segundos > 59) {
+				horaValida = false;
+			}
+		} else {
+			horaValida = false;
+		}
+		return horaValida;
+	}
+}
---
+++ src/examen/HoraTest.java
@@ -0,0 +1,14 @@
+package examen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HoraTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
